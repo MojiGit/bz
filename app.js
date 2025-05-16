7d33a684ad37a6d1f725fdabdd08e60439145f69
Moji <m.mojica.mateus@gmail.com>
2025-05-16 12:40:49 +0200

    structure finished - changing navbar

--- app.js
+++ app.js
@@ -15,3 +15,12 @@ const carousel = document.getElementById('carousel');
       currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
       updateCarousel();
     }
+
+
+const menuButton = document.getElementById('mobile-menu-button');
+const menu = document.getElementById('mobile-menu');
+
+  menuButton.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+  });
+
