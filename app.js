26f31c26980da7eb0e166e17ab3a661f7412e99f
Moji <m.mojica.mateus@gmail.com>
2025-05-23 12:35:54 +0200

    more tokens added, change position

--- app.js
+++ app.js
@@ -1,21 +1,4 @@
-/* const carousel = document.getElementById('carousel');
-    const totalSlides = carousel.children.length;
-    let currentSlide = 0;
 
-    function updateCarousel() {
-      carousel.style.transform = `translateX(-${currentSlide * 100}%)`;
-    }
-
-    function nextSlide() {
-      currentSlide = (currentSlide + 1) % totalSlides;
-      updateCarousel();
-    }
-
-    function prevSlide() {
-      currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
-      updateCarousel();
-    }
-*/
 const track = document.getElementById('carousel-track');
 const slides = track.children;
 const dots = document.querySelectorAll('.dot');
@@ -94,7 +77,7 @@ for (let i = 0; i < 2; i++) {
 
 async function fetchPrices() {
   try {
-    const ids = ['bitcoin', 'ethereum', 'matic-network', 'polkadot', 'solana'];
+    const ids = ['bitcoin', 'ethereum', 'matic-network', 'polkadot', 'solana', 'cardano', 'dogecoin', 'litecoin', 'ripple', 'chainlink', 'stellar', 'uniswap', 'bitcoin-cash', 'monero', 'tron', 'cosmos', 'algorand', 'vechain', 'filecoin', 'aave'];
     const url = `https://api.coingecko.com/api/v3/simple/price?ids=${ids.join(',')}&vs_currencies=usd`;
     const res = await fetch(url);
     const data = await res.json();
