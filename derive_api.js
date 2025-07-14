a41462681ac33b59d819f64f9cff348012bd9ef2
Moji <m.mojica.mateus@gmail.com>
2025-07-14 15:23:44 +0200

    adding filtering functions, date and strike

--- derive_api.js
+++ derive_api.js
@@ -37,7 +37,45 @@ export const getInstrumentsByType = (type) => {
   return instruments.filter(instrument => instrument.option_details.option_type === type);
 };
 
-getInstruments().then(() => {
-  const calls = getInstrumentsByType('C');
-  console.log(calls); // Only options with type 'C'
+export const getInstrumentsByDate = (date) => {
+  return instruments.filter(instrument => {
+    const expiryDate = new Date(instrument.option_details.expiry * 1000);
+    return expiryDate.toISOString().split('T')[0].replace(/-/g, '') === date;
+  });
+}
+
+export async function fetchDatesAndStrikesByType() {
+  await getInstruments();
+
+  // Structure: { C: { date1: [strike1, strike2], ... }, P: { ... } }
+  const result = { C: {}, P: {} };
+
+  for (const instrument of instruments) {
+    const type = instrument.option_details.option_type; // 'C' or 'P'
+    const expiryDate = new Date(instrument.option_details.expiry * 1000)
+      .toISOString()
+      .split('T')[0]
+      .replace(/-/g, '');
+    const strike = instrument.option_details.strike;
+
+    if (!result[type][expiryDate]) {
+      result[type][expiryDate] = new Set();
+    }
+    result[type][expiryDate].add(strike);
+  }
+
+  // Convert Sets to arrays for output
+  for (const type of Object.keys(result)) {
+    for (const date of Object.keys(result[type])) {
+      result[type][date] = Array.from(result[type][date]);
+    }
+  }
+
+  return result;
+}
+
+// Example usage:
+fetchDatesAndStrikesByType().then(res => {
+  console.log('Calls:', res.C); // { '20250725': [strike1, strike2, ...], ... }
+  console.log('Puts:', res.P);  // { '20250725': [strike1, strike2, ...], ... }
 });
